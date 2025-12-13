4e1fc57e0f9c0d1a0a5d64da31067725ca6c0578
EventosAdmValente <eventosadmvalente@gmail.com>
2025-12-13 00:55:57 -0300

    Update back-handler.js

--- back-handler.js
+++ back-handler.js
@@ -1,26 +1,11 @@
 /* back-handler.js
- * Comportamento simples:
- * - Telas internas: volta normalmente
- * - Home: próximo back sai do app
+ * Não interfere no histórico
+ * Apenas existe para compatibilidade futura
  */
-
 (function () {
-
-    const HOME = 'home';
-
     window.BackHandler = {
-
-        onBack(currentScreen) {
-            // Se NÃO estiver na home, deixa o app tratar
-            if (currentScreen !== HOME) {
-                return false;
-            }
-
-            // Se estiver na home, NÃO intercepta
-            // O próximo back será tratado pelo navegador/sistema
-            return false;
+        onBack() {
+            return false; // nunca intercepta
         }
-
     };
-
 })();
