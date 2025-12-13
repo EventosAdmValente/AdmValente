4b0fdb5ba38dc19b47d4f0eb07cc1188634a74a8
EventosAdmValente <eventosadmvalente@gmail.com>
2025-12-13 11:46:13 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,4 @@
-const CACHE_NAME = "adm-valente-v2";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png",
-
-  // CDN ESSENCIAIS
-  "https://cdn.tailwindcss.com",
-  "https://unpkg.com/lucide@latest",
-  "https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap"
-];
-
-// INSTALL
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-// ACTIVATE
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-// FETCH
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => {
-      return res || fetch(event.request);
-    }).catch(() => caches.match("./index.html"))
-  );
+self.addEventListener("install",()=>{});
+self.addEventListener("fetch",e=>{
+ e.respondWith(fetch(e.request));
 });
